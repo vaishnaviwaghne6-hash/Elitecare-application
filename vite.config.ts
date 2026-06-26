97d9fec324803f679a6cb721dda23bdd6c54ac91
Vaishnavi Waghne <vaishnaviwaghne6@gmail.com>
2026-06-26 11:54:56 +0530

    Added base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
+
+
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
+
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+     base: "/Elitecare-application/",
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+  
     server: {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
